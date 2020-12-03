7ab5e5f9909cde8cd9cc507b82c7a55c928399c1
Gabriel TEKOMBO <tekombo.gabriel@gmail.com>
2020-12-03 11:34:31 +0100

    Update type class definition

--- aestheticdialogs/src/main/java/com/thecode/aestheticdialogs/DialogType.kt
+++ aestheticdialogs/src/main/java/com/thecode/aestheticdialogs/DialogType.kt
@@ -2,13 +2,6 @@ package com.thecode.aestheticdialogs
 
 import androidx.annotation.Keep
 
-@Keep
-class DialogType {
-
-    companion object {
-        const val SUCCESS = "DIALOG_SUCCESS"
-        const val ERROR = "DIALOG_ERROR"
-        const val WARNING = "DIALOG_WARNING"
-        const val INFO = "DIALOG_INFO"
-    }
+enum class DialogType {
+    SUCCESS, ERROR, WARNING, INFO
 }
